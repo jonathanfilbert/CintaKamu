dcbb6a77bd96b3018c36429de646e356bc94af23
Matt Livingston <blubrick@gmail.com>
2018-10-04 17:53:09 +1000

    CintaKamu project for HacktoberFest 2018 - Now in Pascal!

---
+++ Code/blubrick.pas
@@ -0,0 +1,17 @@
+(****************************************)
+(* Author:   Matt Livingston            *)
+(*           (aka Blubrick)             *)
+(*                                      *)
+(* Location: Cairns, Australia          *)
+(*           (Far North Queensland)     *)
+(*                                      *)
+(* Purpose:  Print a pleasant message   *)
+(*           in the Indonesian language *)
+(*           (Bahasa Indonesia)         *)
+(*                                      *)
+(****************************************)
+Program CintaKamu;
+
+BEGIN
+	Writeln('Aku Cinta Kamu');
+END.
