94a8de7fd87ff21aef7c55365c21af482e330310
Thomas Mannay <tfm@airmail.cc>
2019-10-08 19:37:49 +0100

    Add Common Lisp

---
+++ Code/sys-fs.lisp
@@ -0,0 +1,10 @@
+;; Thomas Mannay
+;; Wales
+(defpackage aku-cinta-kamu
+  (:use :cl)
+  (:export :main))
+(in-package :aku-cinta-kamu)
+
+(defun main (argv)
+  (declare (ignore argv))
+  (format t "Aku Cinta Kamu~%"))
