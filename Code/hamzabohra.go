f171953a152908d0c8d8a4b93e9730c314617a8c
hamzabohra <71485639+hamzabohra@users.noreply.github.com>
2022-10-02 20:07:08 +0530

    golang language

---
+++ Code/hamzabohra.go
@@ -0,0 +1,17 @@
+// Including the main package
+package main
+  
+// Importing fmt
+import (
+    "fmt"
+)
+  
+// Calling main
+func main() {
+  
+    // Calling print() function
+    fmt.Print("Aku Cinta Kamu")
+  
+   
+  
+}
